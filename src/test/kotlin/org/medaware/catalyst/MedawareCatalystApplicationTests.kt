f597d60e772d65d8761f29e2e613ec933a76973a
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2024-03-13 11:31:55 +0100

    Initial commit: Add spring initializr project

---
+++ src/test/kotlin/org/medaware/catalyst/MedawareCatalystApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.medaware.catalyst
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedawareCatalystApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
