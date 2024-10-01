1ae00d7657e9e672dc8ca7893098c9b43b647133
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2024-10-01 14:35:01 +0200

    Invoke `CorsFilter` on public access routes

--- src/main/kotlin/org/medaware/catalyst/security/PublicAccessFilterChainConfig.kt
+++ src/main/kotlin/org/medaware/catalyst/security/PublicAccessFilterChainConfig.kt
@@ -1,5 +1,6 @@
 package org.medaware.catalyst.security
 
+import org.medaware.catalyst.security.filter.CorsFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered.HIGHEST_PRECEDENCE
@@ -7,6 +8,7 @@ import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 class PublicAccessFilterChainConfig {
@@ -20,6 +22,7 @@ class PublicAccessFilterChainConfig {
             authorizeHttpRequests {
                 authorize(anyRequest, permitAll)
             }
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(CorsFilter())
         }
         return http.build()
     }
